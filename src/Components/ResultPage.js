6caa0f595e26f50e884d75e9114a793713981270
TITHI1244 <39958295+TITHI1244@users.noreply.github.com>
2018-11-28 19:52:16 +0100

    Updated ResultPage component
    Added a 'Try again' button

--- src/Components/ResultPage.js
+++ src/Components/ResultPage.js
@@ -1,18 +1,24 @@
 import React from "react";
-import QuizView from "./QuizView";
+import { Link } 
+    from "react-router-dom";
 
 const ResultPage = ({match}) => (
     <div className="result text-center">
     	{(match.params.score < 5) ? 
     		(<div>
-    			<h1> Good try, {match.params.userName}! </h1> <br />
+    			<h1> Good try, '{match.params.userName}'! </h1> <br />
     		 	<h2>But you can do better.</h2>
     		 </div>) : 
     		(<div>
-    			<h1>Great job, {match.params.userName}! </h1> <br /> 
+    			<h1>Great job, '{match.params.userName}'! </h1> <br /> 
     		 	<h2>You are a star.</h2>
     		</div>)}        
-        <h3>Your score: {match.params.score}</h3>        
+        <h3>Your score: {match.params.score}</h3> <br />
+        <div>
+            <Link to={`/quizView/${match.params.userName}`}>
+                <button className="btn btnAgain">Try Again</button>
+            </Link>
+        </div>                
     </div>
 );
 
