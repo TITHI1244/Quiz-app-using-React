e14e543129958058e537ca2f37b039cf7357b835
TITHI1244 <39958295+TITHI1244@users.noreply.github.com>
2018-11-28 19:38:36 +0100

    Updated Header component
    Added the 'required' name field, while logging in.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,33 +13,38 @@ class Header extends React.Component {
 	getName(e) {
 		let nameOfUser = document.getElementById('nameVal');
 		this.setState({
-			userName: nameOfUser.value
-		})
+				userName: nameOfUser.value
+		})		
 	}
 	render() {
-		return (
-			<div>
+		return (			
 			<div className="start border mt-3 p-2">
-			{(this.state.userName === "") ? 
-				(
-    		<form onSubmit={this.getName}>
-    			<div class="input-group mb-3">
-      				<div class="input-group-prepend">
-        				<span class="input-group-text">Name:</span>
-      				</div>
-      				<input type="text" class="form-control" placeholder="Username" id="nameVal" name="username" />
-    			</div>
-    			<button type="submit" class="btn btn-primary">Submit</button>
-  			</form>
-  		) : (<h1 className="text-center welcome">Welcome {this.state.userName} </h1>)
-			} </div>
-			<div>
-			  	<div className="startBtn">
-					<Link to={`/quizView/${this.state.userName}`}>
-			        	<button className="btn btn-block">Start now</button>
-			        </Link>
-			    </div>
-			</div>
+				{(this.state.userName === "") ? 
+					(
+		    		<form onSubmit={this.getName}>
+		    			<div className="input-group mb-3">
+		      				<div className="input-group-prepend">
+		        				<span className="input-group-text">Name:</span>
+		      				</div>
+		      				<input type="text" 
+		      						class="form-control" 
+		      						placeholder="Please enter your name here..." 
+		      						id="nameVal" 
+		      						name="username" required/>
+		    			</div>
+		    			<button type="submit" class="btn btn-primary">Submit</button>
+		  			</form>
+		  			) : ( <div>
+				  			<h1 className="text-center text-uppercase welcome">
+				  				Welcome '{this.state.userName}'
+				  			</h1>
+				  			<div className="startBtn">
+								<Link to={`/quizView/${this.state.userName}`}>
+						        	<button className="btn btn-block">Start now</button>
+						        </Link>
+						    </div>
+				    	</div> )
+				} 
 			</div>
 			)
 	}
